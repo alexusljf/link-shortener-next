0e48ebbc9a6d6b66ff15121fdb455b2579d535ea
alexusljf <limjinfeng@live.com>
2024-07-22 18:39:25 +0800

    fix: href missing https

--- src/app/listPage/page.tsx
+++ src/app/listPage/page.tsx
@@ -4,13 +4,14 @@ import { headers } from "next/headers";
 
 const Home = () => {
   const domainName = headers().get("host");
+  const baseUrl = `https://${domainName}`;
   return (
     <div className="flex flex-col justify-items-center items-center gap-10 p-10">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Previously Shortened Links
       </h1>
       <LoginDialog />
-      {domainName && <LinkTable domainName={domainName} />}
+      {baseUrl && <LinkTable domainName={baseUrl} />}
     </div>
   );
 };

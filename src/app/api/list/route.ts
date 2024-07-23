70ecc0e3e0aba33bd40582c6ded39a8a6e02fe63
alexusljf <limjinfeng@live.com>
2024-07-23 08:59:36 +0800

    trying to fix

--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -2,21 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 import connect from "../../../../db";
 import Link from "../../../models/Link";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
+  console.log("before finding the records");
   await connect();
   try {
     console.log("before finding the records");
     const record = await Link.find();
     console.log({ record });
     if (record) {
-      return NextResponse.json(record, {
-        status: 200,
-        // headers: {
-        //   "Cache-Control": "no-cache, no-store, must-revalidate",
-        //   Pragma: "no-cache",
-        //   Expires: "0",
-        // },
-      });
+      const response = NextResponse.json(record, { status: 200 });
+      response.headers.set("Cache-Control", "no-store"); // Disable caching
+      return response;
     } else {
       return NextResponse.json(
         { message: "No records found" },

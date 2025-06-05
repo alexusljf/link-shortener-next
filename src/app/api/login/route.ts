fc2e0fb8beec7a7bfcd2651b629a6274b441bf90
alexusljf <limjinfeng@live.com>
2025-06-05 18:19:21 +0800

    feat/bcrypt-hashing

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,35 +1,26 @@
-import { NextRequest, NextResponse } from "next/server";
-import connect from "../../../../db";
-import User from "../../../models/User";
+import {NextRequest, NextResponse} from "next/server"
+import connect from "../../../../db"
+import User from "../../../models/User"
+import bcrypt from "bcryptjs"
 
 export async function POST(req: NextRequest) {
-  await connect();
-  try {
-    const { username, password } = await req.json();
-    console.log({ username, password });
-    const user = await User.findOne({ userName: username });
-    console.log("User found:", user);
-    if (!user) {
-      return NextResponse.json(
-        { message: "User does not exist" },
-        { status: 400 }
-      );
-    }
-    const passwordMatch = user.password === password;
-    if (!passwordMatch) {
-      return NextResponse.json(
-        { message: "Invalid Password, please try again" },
-        { status: 400 }
-      );
-    }
-    return NextResponse.json(
-      { message: "Login Successful", user },
-      { status: 200 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to delete data" },
-      { status: 500 }
-    );
-  }
+	await connect()
+	try {
+		const {username, password} = await req.json()
+		console.log({username, password})
+		const user = await User.findOne({userName: username})
+		if (!user) {
+			return NextResponse.json({message: "User does not exist"}, {status: 400})
+		}
+		const passwordMatch = await bcrypt.compare(password, user.password)
+		if (!passwordMatch) {
+			return NextResponse.json(
+				{message: "Invalid Password, please try again"},
+				{status: 400}
+			)
+		}
+		return NextResponse.json({message: "Login Successful", user}, {status: 200})
+	} catch (error) {
+		return NextResponse.json({error: "Failed to delete data"}, {status: 500})
+	}
 }
